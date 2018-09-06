24f2ddb0512fa6d92c7a0348e88d02d084718628
Anthony Okechukwu - PC <kokoshaggy@yahoo.com>
2018-09-06 11:54:06 +0100

    - missing config

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,9 +1,9 @@
 <?php
 $params = array_merge(
-    require __DIR__ . '/../../common/config/params.php',
-    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
 );
 
 return [
@@ -16,9 +16,17 @@ return [
             'csrfParam' => '_csrf-frontend',
         ],
         'user' => [
-            'identityClass' => 'common\models\User',
+			'class' => 'boffins_vendor\classes\UserComponent',
+            'identityClass' => 'frontend\models\UserDb',
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+			'enableSession' => true,
+			'authTimeout' => 21600, //6 hours 
+			'absoluteAuthTimeout' => 86400, //24 hours 
+			'on '.\yii\web\User::EVENT_BEFORE_LOGIN => ['frontend\models\UserDb', 'handleBeforeLogin'],
+			'on '.\yii\web\User::EVENT_AFTER_LOGIN => ['frontend\models\UserDb', 'handleAfterLogin'],
+			'on '.\yii\web\User::EVENT_BEFORE_LOGOUT => ['frontend\models\UserDb', 'handleBeforeLogout'],
+			'on '.\yii\web\User::EVENT_AFTER_LOGOUT => ['frontend\models\UserDb', 'handleAfterLogout'],
         ],
         'session' => [
             // this is the name of the session cookie used for login on the frontend
@@ -36,6 +44,27 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+		'authManager' => [
+			'class' => 'yii\rbac\PhpManager',
+        ],     
+		'settingscomponent' => [
+        	'class' => 'frontend\settings\components\Settingsdate',
+    	],
+		'i18n' => [
+        'translations' => [
+            'app*' => [
+                'class' => 'yii\i18n\PhpMessageSource',
+                //'basePath' => '@app/messages',
+                //'sourceLanguage' => 'en-US',
+                'fileMap' => [
+                    'app' => 'app.php',
+                    'app/error' => 'error.php',
+                ],
+            ],
+        ],
+    ],
+	
+   
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
@@ -45,5 +74,50 @@ return [
         ],
         */
     ],
+
+	'modules' => [
+    'settings' => [
+        'class' => 'frontend\settings\Module',
+		],
+	],
+
+	'defaultRoute' => 'site/login',
+
     'params' => $params,
+	'as beforeRequest' => [
+		'class' => 'yii\filters\AccessControl',
+		'rules' => [
+			[
+				'allow' => true,
+				//'controllers' =>['project'],
+				'actions' => ['login'],
+				'roles' => ['?'],
+			],
+			[
+				'allow' => true,
+				'controllers' =>['api/project'],
+				
+				  
+			],
+			[
+				'allow' => true,
+				'roles' => ['@'],
+			],
+			/*[
+				'allow' => true,
+				'roles' => ['?'],
+			],*/
+		],
+		'denyCallback' => function () {
+			$comingFrom = ['project/view'];
+			$comingFrom = $comingFrom[0] == 'site/login' || empty($comingFrom[0]) ? [Yii::$app->defaultRoute] : $comingFrom;
+			Yii::$app->request->isPjax || Yii::$app->request->isAjax ? Yii::$app->session->set( 'comingFrom', [Yii::$app->defaultRoute] ) : Yii::$app->session->set( 'comingFrom', $comingFrom );
+			return Yii::$app->response->redirect(['site/login', 'test' => 2]);
+		},
+	],
+
 ];
+
+
+
+
