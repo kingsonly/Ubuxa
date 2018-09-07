b00957c722f629b03ecee6b4840d8b18d26dec9a
Anthony Okechukwu - PC <kokoshaggy@yahoo.com>
2018-09-07 05:52:28 +0100

    *Configuring test environment

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,7 @@
 $db = require(__DIR__ . '/db.php');
 $db_backup = require(__DIR__ . '/db_backup.php');
 $db_tenant = require(__DIR__ . '/db_tenant.php');
+$db_test = require(__DIR__ . '/testDb.php');
 $transport = require(__DIR__ . '/transport.php');
 
 return [
@@ -16,7 +17,7 @@ return [
 		],
 		'db' => $db,
 		'db_backup' => $db_backup,
-		 'db_tenant' => $db_tenant, 
+		'db_tenant' => $db_tenant, 
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
@@ -29,6 +30,23 @@ return [
         ],
 		'global' => [
 			'class' => 'boffins_vendor\classes\GlobalComponent',
-		]
+		],
     ],
+	'controllerMap' => [
+		// Common migrations for the whole application
+		'migrate' => [
+			'class' => 'yii\console\controllers\MigrateController',
+			'migrationNamespaces' => ['console\migrations'],
+			'migrationTable' => 'migration',
+			'migrationPath' => null,
+		],
+		// Migrations for testing only 
+		'migrate-test' => [
+			'class' => 'yii\console\controllers\MigrateController',
+			'migrationNamespaces' => ['common\tests\migrations'],
+			'migrationTable' => '{{%migration}}',
+			'migrationPath' => null,
+			'db' => $db_test
+		],
+	],
 ];
