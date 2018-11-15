fefcc0cf86befe2873b7ee95e8ddf6b86be4e312
Anthony Okechukwu - PC <kokoshaggy@yahoo.com>
2018-11-15 18:19:57 +0100

    * collection classes

---
+++ boffins_vendor/classes/Collection.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019 Ubuxa (By Epsolun Ltd)
+ */
+
+namespace boffins_vendor\classes;
+
+use Yii;
+use ArrayAccess;
+use ArrayIterator;
+use IteratorAggregate;
+use Countable;
+use yii\base\Component;
+use yii\base\StaticInstanceInterface;
+use yii\base\Arrayable;
+use yii\base\StaticInstanceTrait;
+use yii\base\ArrayableTrait;
+use yii\base\ArrayAccessTrait;
+use yii\base\InvalidCallException;
+use yii\base\InvalidArgumentException;
+
+/**
+ * This is an Special class to collect a set of objects. 
+ * This is a read/write collection. However, a warning is triggered when trying to 'manualy' set the items. 
+ * It uses an array (associative or not)
+ */
+class Collection extends Component implements StaticInstanceInterface, IteratorAggregate, ArrayAccess, Arrayable, Countable
+{
+	use ArrayableTrait;
+	use ArrayAccessTrait;
+    use StaticInstanceTrait;
+
+	/*
+	 * array that holds the actual items   
+	 */
+	private $_items;
+	
+	/***
+	 *  @brief constructor 
+	 *  
+	 *  @param $items = the items you want in the collection. You can start with an empty collection 
+	 *  and add items one at a time. 
+	 *  @param $config = configuration variable. As per Yii's format for classes that extend from BaseObject, 
+	 *  you can use $config to set public variables of the class.
+	 */
+	public function __construct(array $items = [], $config = [])
+    {
+        $this->_items = $items;
+        parent::__construct($config);
+    }
+	
+	/***
+	 *  @brief returns the data held in the collection. 
+	 *  This is a simple getter.
+	 *  
+	 *  @return array of items in the collection. 
+	 */
+	public function getItems() 
+	{
+		if ( empty($this->_items) ) {
+			Yii::trace("This collection is empty. " . static::className() );
+		}			
+		return $this->_items;
+	}
+	
+	/**
+	 *  @brief next two properties created in the hope that they fulfil the requirements necessary 
+	 *  to use ArrayableTrait
+	 */
+	public function getData()
+	{
+		return $this->getItems();
+	}
+	
+	public function setData($data)
+	{
+		$this->setItems($data);
+	}
+	
+	/***
+	 *  @brief mass assign all the items you want into the collection 
+	 *  so they act like a collection now. 
+	 *  
+	 *  @param [array] $items you want to set
+	 *  @return mixed. the array you just set lol. Or false if the function failed. 
+	 *  
+	 *  @details If the collection is not empty, or the argument provided is not an array, or it is empty, 
+	 *  this triggers an error and asks you to use the correct function instead
+	 */
+	public function setItems($items) 
+	{
+		if ( !empty($this->_items) ) {
+			throw new InvalidCallException( "You can not reset the items in a collection. You can refresh and then addItem or  addItems " . static::className() );
+		}
+		
+		if ( !is_array($items) ) {
+			throw new InvalidArgumentException( "You can only mass assign an array to a collection. If you want a mixed collection, use  addItem." . static::className() ); 
+		}
+		
+		if ( empty($items) ) {
+			throw new InvalidCallException( "To clear items, use emptyCollection() " . static::className() );
+		}
+		
+		$this->_items = $items;
+	}
+	
+	
+	/***
+	 *  @brief function to add an item to the collection
+	 *  
+	 *  @param [mixed] $item the item to add to the collection
+	 *  @param [string|integer] $key a key to use to access the item.
+	 *  @return the collection
+	 */
+	public function addItem($item, $key = '')
+	{
+		if ( empty($key) ) {
+			$this->_items[] = $item;
+			return $this;
+		} else {
+			if ( is_string($key) || is_integer($key) ) {
+				$this->_items[$key] = $item;
+				return $this;
+			}
+			throw new InvalidArgumentException( "Second Argument 'key' must be string or integer" . static::className() ); 
+		}
+	}
+	
+	/***
+	 *  @brief add an iteratable object to the collection
+	 *  
+	 *  @param [array|collection|Object instanceof Traversable] $items has to be an iteratable variable
+	 *  @param [string|integer] a key  to place all the items under. 
+	 *  @return the collection
+	 *  
+	 *  @details $items must be iterable i.e. implements, Traversable/IteratorAggregate (PHP 7>=7.1)
+	 */
+	public function addItems( $items, $commonKey = '' )  
+	{
+		$traversable = false;
+		if ( ! function_exists('is_iterable') ) {
+			if ( is_array($items) || (is_object($items) && $items instanceof Traversable ) ) {
+				$traversable = true;
+			}
+		} elseif ( is_iterable($items) ) {
+			$traversable = true;
+		}
+		
+		if ($traversable) {
+			foreach ($items as $singleItem) {
+				$this->addItem($singleItem, $commonKey);
+			}
+			return $this;
+		}
+		
+		throw new InvalidArgumentException( "Argument one, 'items', must be traversable. Arrays, collections or objects implementing Traversable" . static::className() ); 
+	}
+	
+	/**
+	 *  @brief remove a single item from the collection
+	 *  
+	 *  
+	 *  @param [integer|string] $key requires a key to remove the item from the collection 
+	 *  @return the collection
+	 *  @detail throws an exception if the key does not exist or key provided is not an integer or string. 
+	 */
+	public function removeItem($key) 
+	{
+		if( is_string($key) || is_integer($key) ) {
+			if ( array_key_exists( $key, $this->getItems() ) ) {
+				unset( $this->_item[$key] );
+				return $this;
+			}
+			throw new InvalidArgumentException( "This key does not exist in this collection" . static::className() ); 
+		} else {
+			throw new InvalidArgumentException( "Argument 'key' must be string or integer" . static::className() ); 
+		}
+		
+	}
+	
+	/**
+	 *  @brief tell if the collection is empty. 
+	 *  
+	 *  @return boolea
+	 */
+	public function isEmpty() 
+	{
+		return empty($this->getItems());
+	}
+	
+	/***
+	 *  @brief function to retrieve all the values of the items in the collection
+	 *  without the keys. (is this useful?)
+	 *  
+	 *  @return collection. values of the items in the collection
+	 */
+	public function values()
+    { 
+        return new static( array_values($this->getItems()) );
+    }
+	
+	/**
+	 *  @brief function to retrieve the keys of the items in the collection
+	 *  
+	 *  @return collection the keys of the original collection
+	 */
+	public function keys()
+    {
+        return new static(array_keys($this->getItems()));
+    }
+	
+	/**Run a function/callback against all items in the collection.
+     *
+     * The original collection will not be changed, a new collection with modified data is returned.
+     * @param $function the callback function to apply. Signature: `function($model)`.
+     * @return static a new collection with items returned from the callback.
+     */
+    public function map($function)
+    {
+        array_map($function, $this->getItems());
+		return $this;
+    }
+}
---
+++ boffins_vendor/classes/ModelCollection.php
@@ -0,0 +1,282 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019 Ubuxa (By Epsolun Ltd)
+ */
+ 
+namespace boffins_vendor\classes;
+
+use yii\db\ActiveQuery;
+use yii\db\ActiveRecordInterface;
+use yii\base\Model;
+use yii\db\BaseActiveRecord;
+use yii\helpers\Json;
+use yii\base\InvalidCallException;
+use yii\base\InvalidArgumentException;
+
+
+/**
+ * ModelCollection
+ *
+ * @authors - Originally written by Carsten Brandt <mail@cebe.cc>
+ * @authors - Heavily modified by Anthony Okechukwu and Rustam Mamadaminov
+ * @since 1.0
+ */
+class ModelCollection extends Collection
+{
+    /**
+     * @var ActiveQuery|null the query that returned this collection.
+     * May be`null` if the collection has not been created by a query.
+     */
+    public $query = null;
+	
+	/***
+	 *  @var indicates that this collection must be based on a query ONLY. 
+	 *  if the collection can use a mix of query and and dynamically added items
+	 *  then you must set $queryString to false. 
+	 */
+	public $queryStrict = true;
+	
+	/***
+	 *  set the model class this collection is for - if empty, any model will be accepted.
+	 */
+	public $acceptableModelClass = '';
+	
+	/**
+     * Lazy evaluation of models, if this collection has been created from a query.
+     */
+    public function getData()
+    {
+		if ( $this->usesQuery() && $this->_items === null ) { //check if getModels() is empty - 
+																//note that models can return [] after query but is not null
+			$this->setItems() = $this->queryAll(); //items are already empty so setItems won't throw an exception
+        }
+        return parent::getData();
+    }
+    /**
+     * @return array|BaseActiveRecord[]|ActiveRecordInterface[]|Arrayable[] models contained in this collection.
+     */
+    public function getModels()
+    {
+        return $this->getData();
+    }
+	
+    // TODO relational operations like link() and unlink() sync()
+    // https://github.com/yiisoft/yii2/pull/12304#issuecomment-242339800
+    // https://github.com/yiisoft/yii2/issues/10806#issuecomment-242346294
+    // TODO addToRelation() by checking if query is a relation
+    // https://github.com/yiisoft/yii2/issues/10806#issuecomment-241505294
+    // https://github.com/yiisoft/yii2/issues/12743
+    public function findWith($with)
+    {
+        if ( !$this->usesQuery() ) {
+            throw new InvalidCallException('This collection was not created from a query, so findWith() is not possible.');
+        }
+        $this->query->findWith($with, $this->getModels());
+        return $this;
+    }
+
+    /**
+     *  @brief delete all models in the collection
+     *  
+     *  @return void
+     */
+    public function deleteAll()
+    {
+        foreach($this->getModels() as $model) {
+            $model->delete();
+        }
+    }
+	
+    /**
+     *  @brief change the scenario of the models
+     *  
+     *  @param [string] $scenario the new scenaario
+     *  @return $this collection
+     */
+    public function scenario($scenario)
+    {
+        foreach($this->getModels() as $model) {
+            $model->scenario = $scenario;
+        }
+        return $this;
+    }
+    /**
+     * https://github.com/yiisoft/yii2/issues/13921
+     *
+	 * @param array $attributes
+     * @param bool $safeOnly
+     * @param bool $runValidation
+     * @return ModelCollection
+	 * TOD - trigger an event 
+	 */
+    public function updateAll($attributes, $safeOnly = true, $runValidation = true)
+    {
+        foreach($this->getModels() as $model) {
+            $model->setAttributes($attributes, $safeOnly);
+            $model->update($runValidation, array_keys($attributes));
+        }
+        return $this;
+    }
+	
+	/***
+	 * @brief  
+	 *
+     * @param array $attributes
+     * @param bool $safeOnly
+     * @param bool $runValidation
+     * @return $this
+     */
+    public function insertAll($attributes, $safeOnly = true, $runValidation = true)
+    {
+        foreach ($this->getModels() as $model) {
+            $model->setAttributes($attributes, $safeOnly);
+            $model->insert($runValidation, array_keys($attributes));
+        }
+        return $this;
+    }
+	
+	public function fillAll($attributes, $safeOnly = true)
+    {
+        foreach ($this->getModels() as $model) {
+            $model->setAttributes($attributes, $safeOnly);
+        }
+        return $this;
+    }
+	
+	public function loadModel($key, $attributes, safeOnly = true)
+	{
+		$models = $this->getModel();
+		$model = $models[$key];
+		$model->setAttributes($attributes, $safeOnly);
+	}
+	
+	
+    public function saveAll($runValidation = true, $attributeNames = null)
+    {
+        foreach ($this->getModels() as $model) {
+            $model->save($runValidation, $attributeNames);
+        }
+        return $this;
+    }
+	
+	public function saveModel($key, $runValidation = true, $attributeNames = null)
+	{
+		$models = $this->getModel();
+		$model = $models[$key];
+		$model->save($runValidation, $attributeNames);
+	}
+    /**
+     * https://github.com/yiisoft/yii2/issues/10806#issuecomment-242119472
+     *
+     * @return bool
+     */
+    public function validateAll()
+    {
+        $success = true;
+        foreach($this->getModels() as $model) {
+            if (!$model->validate()) {
+                $success = false;
+            }
+        }
+        return $success;
+    }
+	
+	public function validateModel($key)
+	{
+		$models = $this->getModel();
+		$model = $models[$key];
+		return $model->validate();
+	}
+    /**
+     * @param array $fields
+     * @param array $expand
+     * @param bool $recursive
+     * @return Collection|static
+     */
+    public function toArray(array $fields = [], array $expand = [], $recursive = true) //map is not implemented!
+    {
+        return $this->map( function($model) use ($fields, $expand, $recursive) {
+            /** @var $model Arrayable */
+            return $model->toArray($fields, $expand, $recursive) //inline function
+        });
+    }
+    /**
+     * Encodes the collected models into a JSON string.
+     * @param int $options the encoding options. For more details please refer to
+     * <http://www.php.net/manual/en/function.json-encode.php>. Default is `JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE`.
+     * @return string the encoding result.
+     */
+    public function toJson($options = 320)
+    {
+        return Json::encode($this->toArray()->getModels(), $options); //toArray is not implemented because map does not work 
+    }
+	
+	/*
+	 *  @brief function to trigger the query if the model collection was built on a query
+	 *  
+	 *  @return ActiveRecord. An array of the models that match the query.. 
+	 */
+	private function queryAll()
+    {
+        if ($this->query === null) {
+            throw new InvalidCallException('This collection was not created from a query.');
+        }
+        return $this->query->all();
+    }
+	
+	private function _ensureModels()
+    {
+		if ( $this->usesQuery() && $this->_items === null ) { //check if getModels() is empty - 
+																//note that models can return [] after query but is not null
+			$this->setItems() = $this->queryAll(); //items are already empty so setItems won't throw an exception
+        }
+    }
+	
+	/**
+	 *  @brief common function to check if this model collection strictly uses queries and actually has a query set at this point
+	 *  
+	 *  @return boolean
+	 */
+	public function usesQuery()
+	{
+		return $this->queryStrict ? !($this->query === null || $this->query === false) : false;
+	}
+	
+	/***
+	 *  @inheritdoc
+	 *  
+	 *  overriding to ensure that you cannot add an item when the collection is created through a query. 
+	 */
+	public function addItems( $items, $commonKey = '' )
+	{
+		if ( $this->usesQuery() ) 
+		{
+			throw new InvalidCallException('This collection was created from a query. And you cannot add items to it.');
+		}
+		return parent::addItems($items, $commonKey);
+	}
+	
+	/***
+	 *  @inheritdoc
+	 *  
+	 *  overriding to ensure that you cannot add an item when the collection is created through a query. 
+	 */
+	public function addItem( $item, $key = '' )
+	{
+		if ( $this->usesQuery() ) 
+		{
+			throw new InvalidCallException('This collection was created from a query. And you cannot add items to it.');
+		}
+		
+		if ( ! $item instanceof Model ) {
+			throw new InvalidArgumentException( "Argument 'item' must be a member of yii\base\Model" . static::className() ); 
+		}
+		
+		if ( !empty($this->acceptableModelClass) && !$item instanceof Model $this->acceptableModelClass ) {
+			throw new InvalidArgumentException( "Argument 'item' must be a member of {$this->acceptableModelClass}" . static::className() ); 
+		}
+		return parent::addItem($item, $key);
+	}
+	
+
+}
