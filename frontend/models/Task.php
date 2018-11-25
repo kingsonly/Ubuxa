c40ddb138b9eda93590c0d933b7adbc2f6c28c72
Emeka Kanikwu <kanikwuemeka@gmail.com>
2018-11-25 15:02:34 +0100

    task time elapsed fix

--- frontend/models/Task.php
+++ frontend/models/Task.php
@@ -266,4 +266,33 @@ class Task extends BoffinsArRootModel implements TenantSpecific, TrackDeleteUpda
 
         return $this->formatInterval($timeTaken);
     }
+
+    public function getTimeElapsedString($full = false) {
+        $now = new \DateTime();
+        $ago = new \DateTime($this->create_date);
+        $diff = $now->diff($ago);
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'year',
+            'm' => 'month',
+            'w' => 'week',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' ago' : 'just now';
+    }
 }
