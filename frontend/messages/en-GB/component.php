02072aaf3c65f6e945aff69e8b76756dc6846acf
Anthony Okechukwu - PC <kokoshaggy@yahoo.com>
2018-11-28 14:56:46 +0100

    - component language file

---
+++ frontend/messages/en-GB/component.php
@@ -0,0 +1,13 @@
+<?php 
+
+/****
+ * TRANSLATION MAP FOR ENGLISH UK. ('THE SOURCE')
+ */
+
+return [
+	'value_saved' => "The value was saved into the DB successfully",
+	'value_not_saved' => "Sorry. The value was NOT saved",
+	'value_true' => "Yes",
+	'value_false' => "No",
+];
+?>
