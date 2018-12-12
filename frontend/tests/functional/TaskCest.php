c800d73d5ff61a9452e1311d186d6633b185e403
DESKTOP-DO0AR9I\RADIANCE <godswillogundu@yahoo.com>
2018-12-12 12:00:55 +0100

    commit

--- frontend/tests/functional/TaskCest.php
+++ frontend/tests/functional/TaskCest.php
@@ -7,7 +7,10 @@ class TaskCest
 {
     public function _before(FunctionalTester $I)
     {
-       
+        $I->amOnRoute('site/login');
+        $admin = \frontend\models\UserDb::findById('guest');
+        $I->amLoggedInAs($admin);
+       // $I->amOnPage('folder/index');
     }
 
     public function _after(FunctionalTester $I)
@@ -18,9 +21,10 @@ class TaskCest
     
     public function tryToTest(FunctionalTester $I)
     {
-        
-         $I->amOnRoute('folder/index');
-         $I->seeInCurrentUrl('folder/index');
-         //$I->see('login');
+         $I->amOnPage('folder/index');
+         $I->seeInCurrentUrl('folder%2Findex');
+         $I->see('test folder');
+         $I->click('test folder');
+         //$I->see('TASKS');
     }
 }
