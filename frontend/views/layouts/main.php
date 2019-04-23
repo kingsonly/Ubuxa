61bebc5295b60f936d1f98ec8b32370257865cc8
kingsley achumie <kingsonly13c@gmail.com>
2019-04-23 14:48:39 +0100

    Folder api modifications

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -142,10 +142,46 @@ $waitToLoad = Yii::$app->settingscomponent->boffinsLoaderImage($size = 'md', $ty
 
           
         </div>
+		  
+		  <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- Messages: style can be found in dropdown.less-->
+          <li class="dropdown messages-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-envelope-o"></i>
+              <span class="label label-success">4</span>
+            </a>
+            <ul class="dropdown-menu">
+              <li class="header">You have 4 messages</li>
+              <li>
+                <!-- inner menu: contains the actual data -->
+                <ul class="menu">
+                  <li><!-- start message -->
+                    <a href="#">
+                      <div class="pull-left">
+                        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                      </div>
+                      <h4>
+                        Support Team
+                        <small><i class="fa fa-clock-o"></i> 5 mins</small>
+                      </h4>
+                      <p>Why not buy a new awesome theme?</p>
+                    </a>
+                  </li>
+                  <!-- end message -->
+                </ul>
+              </li>
+              
+            </ul>
+          </li>
+          
+        </ul>
+      </div>
 
         
       </div>
       <!-- /.container-fluid -->
+		
     </nav>
   </header>
 	
