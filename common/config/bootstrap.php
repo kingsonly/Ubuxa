02aac78c9c61c6e6b4fbb42e89558f7614004197
kingsley achumie <kingsonly13c@gmail.com>
2019-08-14 01:09:34 +0100

    ob_start removed from common bootstrap

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -5,4 +5,4 @@ Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@boffins_vendor', dirname(dirname(__DIR__)) . '/boffins_vendor');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
-  ob_start ();
+  //ob_start ();
